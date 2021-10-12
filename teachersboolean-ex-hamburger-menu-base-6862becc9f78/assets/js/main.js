0dcfa9e5380c7d51adc85cd8d9c59670f6ca4ac2
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-12 16:39:13 +0200

    alert di prova su hamburger menu

---
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/assets/js/main.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelectorAll("i.fa-bars")
+const close = document.querySelectorAll("a.close")
+/* / variabili */
+
+hamburger.addEventListener("toggle", funzione);
+
+function funzione() {
+    alert("hey");
+}
